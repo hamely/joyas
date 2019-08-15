8e049e6dde198a8d98dbfc66a6a27105d956796a
hamely <sarmiento101147@gmail.com>
2019-08-15 17:59:36 -0500

    sd
    sad

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,120 @@
+<footer class="row">
+            <div class="row m0 topFooter">
+               <!--             <div class="container line1">
+                  <div class="row footFeatures">
+                      <div class="col-sm-4 footFeature">
+                          <div class="media">
+                              <div class="media-left icon"><img src="images/icons/car3.png" alt=""></div>
+                              <div class="media-body texts">
+                                  <h4>free shipping</h4>
+                                  <h2>International</h2>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-sm-4 footFeature">
+                          <div class="media m0">
+                              <div class="media-left icon"><img src="images/icons/tel24-7_2.png" alt=""></div>
+                              <div class="media-body texts">
+                                  <h4>24 hours helpline</h4>
+                                  <h2>123 456 789</h2>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-sm-4 footFeature">
+                          <div class="media m0">
+                              <div class="media-left icon"><img src="images/icons/shopping-bag2.png" alt=""></div>
+                              <div class="media-body texts">
+                                  <h4>see our</h4>
+                                  <h2>latest offers</h2>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  </div> -->
+               <div class="container line2">
+                  <div class="row">
+                     <div class="col-sm-3 widget">
+                        <div class="row m0">
+                           <h4>About Dshine</h4>
+                           <p>We provide the best Quality of products to you.We are always here to help our lovely customers.</p>
+                           <ul class="list-inline">
+                              <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                              <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                              <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                              <li><a href="#"><i class="fab fa-google-plus"></i></a></li>
+                           </ul>
+                        </div>
+                     </div>
+                     <div class="col-sm-3 widget">
+                        <div class="row m0">
+                           <h4>information</h4>
+                           <ul class="nav">
+                              <li><a href="about.html">About Us</a></li>
+                              <li><a href="blog.html">Blog</a></li>
+                              <li><a href="#">Careers</a></li>
+                              <li><a href="#">Top Brands</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                     <div class="col-sm-3 widget">
+                        <div class="row m0">
+                           <h4>top brands</h4>
+                           <ul class="nav">
+                              <li><a href="#">Fusce iaculis</a></li>
+                               <li><a href="#">Nisl dapibus</a></li>
+                               <li><a href="#">Vulputate sapien</a></li>
+                               <li><a href="#">Aliquet</a></li>
+                               <li><a href="#">Risus feugiat</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                     <div class="col-sm-3 widget">
+                        <div class="row m0">
+                           <h4>subscribe to our latest news</h4>
+                           <form action="subscriptionList.php" method="post" role="form">
+                              <div class="input-group">
+                                 <span class="input-group-addon"><i class="fas fa-envelope"></i></span>
+                                 <input type="email" class="form-control" id="subscribeEmail" name="subscribeEmail" placeholder="EMAIL ADDRESS">
+                              </div>
+                              <input type="submit" class="form-control" value="Subscribe" id="submit" name="submit">
+                           </form>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="row m0 copyRight">
+               <div class="container">
+                  <div class="row">
+                     <div class="fleft">&copy; 2018 <a href="index.html">Dshine</a> All Rights Reserved.</div>
+                     <ul class="nav nav-pills fright">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="about.html">about</a></li>
+                        <li><a href="blog.html">blog</a></li>
+                        <li><a href="contact.html">contact</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </footer>
+      </div>
+      <!--jQuery, Bootstrap and other vendor JS-->
+      <!--jQuery-->
+      <script src="<?php echo base_url('assets/js/jquery-2.1.3.min.js'); ?>"></script>
+      <!--Google Maps-->
+      <script src="https://maps.googleapis.com/maps/api/js'); ?>"></script>
+      <!--Bootstrap JS-->
+      <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+      
+      <!--Owl Carousel-->
+      <script src="<?php echo base_url('assets/vendors/isotope/owl.carousel.min.js'); ?>"></script>
+      <!--Isotope-->
+      <script src="<?php echo base_url('assets/vendors/isotope/isotope-custom.js'); ?>"></script>
+      <!--FlexSlider-->
+      <script src="<?php echo base_url('assets/vendors/isotope/jquery.flexslider-min.js'); ?>"></script>
+      <!--D-shine JS-->
+      <script src="<?php echo base_url('assets/js/d-shine.js'); ?>"></script>
+   </body>
+</html>
+
---
+++ application/views/layouts/header.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>D-Shine- Jewellery, Diamond Business/shop Template</title>
+      <!--Favicons-->
+      <link rel="icon" type="assets/image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+      <meta name="msapplication-TileColor" content="#ffffff">
+      <meta name="theme-color" content="#ffffff">
+      <!--Bootstrap and Other Vendors-->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-theme.min.css'); ?>">
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/fontawesome-all.min.css'); ?>">
+      <link rel="stylesheet" href="<?php echo base_url('assets/vendors/owl.carousel/css/owl.carousel.min.css'); ?>">
+       
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/vendors/flexslider/flexslider.css'); ?>" media="screen" />
+      <!--Fonts-->
+      <!--  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800|Montserrat:400,700' rel='stylesheet' type='text/css'> -->
+      <!--     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">  -->
+      <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+      <!--Mechanic Styles-->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/responsive.css'); ?>">
+      <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+      <![endif]-->
+   </head>
+   <body class="home-2">
+      <a href="#" id="back-to-top" title="Back to top">&uarr;</a>
+      <div class="innerLayout">
+         <header class="row" id="header2">
+            <div class="container">
+               <div class="row">
+                  <div class="col-sm-12 col-md-5 menu2 pull-right">
+                     <ul class="nav fright">
+                        <li><a href="#">INFORMACIÓN IMPORTANTE</a></li>
+                        <li><a href="#">CONTACTO</a></li>
+                        <li><img src="<?php echo base_url('assets/images/icons/face.png'); ?>" style="height: 50px;margin-top: -15px;"></li>
+                        <li><img src="<?php echo base_url('assets/images/icons/tu.png'); ?>" style="height: 50px;margin-top: -15px;"></li>
+
+                        <!-- <li><a data-toggle="collapse" href="#searchForm" aria-expanded="false" aria-controls="searchForm"><i class="fas fa-search"></i></a></li> -->
+                       <!--  <li><a href="cart.html"><span class="badge">2</span></a></li> -->
+                     </ul>
+                  </div>
+                  <div class="col-sm-12 col-md-2 logo text-center pull-right">
+                     <a href="index2.html"><img src="<?php echo base_url('assets/images/logo.png'); ?>" alt="D-shine"></a>
+                  </div>
+                  <div class="col-sm-12 col-md-5 menu ">
+                     <nav class="navbar navbar-default navbar-static-top m0">
+                        <div class="row m0">
+                           <!-- Brand and toggle get grouped for better mobile display -->
+                           <div class="navbar-header">
+                              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainNav2">
+                              <i class="fas fa-bars"></i>
+                              </button>
+                           </div>
+                           <!-- Collect the nav links, forms, and other content for toggling -->
+                           <div class="collapse navbar-collapse" id="mainNav2">
+                              <ul class="nav navbar-nav">
+                                 <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">INTRODUCCIÓN</a>
+                                    
+                                 </li>
+                                 <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">NOSOTROS</a>
+                                    
+                                 </li>
+                                 <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">CATÁLOGO</a>
+                                    <ul class="dropdown-menu" role="menu">
+                                       <li><a href="#">Oro blanco</a></li>
+                                       <li><a href="#">Oro combinado</a></li>
+                                       <li><a href="#">Oro amarillo</a></li>
+                                       <li><a href="#">Oro rojo</a></li>
+                                    </ul>
+                                 </li>
+                                 <li><a href="index.php/product">MIEL</a></li>
+                                 <li><a href="<?php echo site_url(''); ?>">PIEDRAS</a></li>
+                                 
+                              </ul>
+                           </div>
+                           <!-- /.navbar-collapse -->
+                        </div>
+                        <!-- /.container-fluid -->
+                     </nav>
+                  </div>
+               </div>
+            </div>
+            <div class="collapse" id="searchForm">
+               <div class="container">
+                  <form action="#" method="get" role="form" class="">
+                     <div class="input-group">
+                        <input type="search" class="form-control" placeholder="Search">
+                        <span class="input-group-addon">
+                        <i class="fas fa-search"></i>
+                        </span>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </header>
